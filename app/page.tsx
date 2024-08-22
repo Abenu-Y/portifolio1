5ace95572cfc30fc106fe79f2f8c0c747072114b
Abenezer Y <abeneyoseph@gmail.com>
2024-08-22 09:01:14 +0300

    add experiece , skills , project and contact section

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,43 @@
+import About from "@/components/About";
+import ContactUs from "@/components/ContactUs";
+import Experience from "@/components/Experience";
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll">
+   <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
    
 
-     {/* header */}
       <Header />
-     {/* hero-section */}
+
      <section id="hero" className="snap-center">
        <Hero />
      </section>
-     {/* about-section */}
-     {/* Experience  */}
-     {/* Skills  */}
 
-     {/* Projects */}
-     {/* Contact Me */}
+     <section id='about' className="snap-center">
+        <About />
+     </section>
+
+     <section id='experience' className="snap-center">
+        <Experience />
+     </section>
+
+     <section id='skills' className="snap-start">
+         <Skills />
+     </section>
+
+     <section id='projects' className="snap-start">
+         <Projects />
+     </section>
+
+     <section id='contact' className="snap-start">
+         <ContactUs />
+     </section>
+
    </main>
   );
 }
